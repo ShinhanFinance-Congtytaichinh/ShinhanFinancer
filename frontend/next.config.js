df8a549ae350081a331b0ded3092409dfe1f9240
ShinhanFinance-Congtytaichinh <iuxinh958@gmail.com>
2025-04-19 05:48:25 +0700

    Delete frontend directory

--- frontend/next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-     const nextConfig = {
-       reactStrictMode: true,
-       images: {
-         domains: ['via.placeholder.com', 'imgur.com', 'www.shinhan.com.vn'],
-       },
-     };
-     module.exports = nextConfig;
